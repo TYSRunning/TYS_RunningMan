7cd894716e8cdb6f0054857840f4ac1590f0d740
zhm <769956207@qq.com>
2016-12-19 17:51:00 +0800

    12-19

--- Assets/Scripts/ChangeVoice.cs
+++ Assets/Scripts/ChangeVoice.cs
@@ -3,13 +3,7 @@ using System.Collections;
 
 public class ChangeVoice : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void slider(){
+		
 	}
 }
---
+++ Assets/Scripts/Main_Menu/Developer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Developer : MonoBehaviour {
+
+	public void Click(){
+
+		Application.LoadLevel ("Developer");//加载场景
+	}
+}
+
